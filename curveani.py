20d791991c928bd955e6613b1444354910e4c3d3
Swayamvijay43 <swayamvijaypagare85@gmail.com>
2024-05-05 22:02:46 +0530

    curve animation
    animation of  a² * y² = x² (2a-x)*(x-a)

--- curveani.py
+++ curveani.py
@@ -4,7 +4,7 @@ class curve(Scene):
     def construct(self):
         a = 1
 
-        question_text = Text("Trace the Curve : a^2 * y^3 = x^2 (2a-x)*(x-a)").scale(0.6)
+        question_text = Text("Trace the Curve : a² * y² = x² (2a-x)*(x-a)").scale(0.6)
         question_text.move_to(ORIGIN)
         self.play(Write(question_text))
         self.play(question_text.animate.shift(UP*3))
@@ -70,11 +70,12 @@ class curve(Scene):
         self.play(Unwrite(intercept))
         self.play(Unwrite(origin))
 
-        tangent = Text("Tangent : ").scale(0.5).to_edge(LEFT * 3)
-        lowest = Text("Equating the lowest degree term to Zero And Differentiating").scale(0.5).next_to(tangent, DR)
-        equ = Text("(a^2 * y^2) + (2 * a^2 * x^2)= 0").scale(0.5).next_to(lowest, DOWN)
-        equ2 = Text("y^2 = -2*x^2").scale(0.5).next_to(equ, DOWN)
-        equ3 = Text("Therefore y is imaginary and there exist no tangent at (0,0)").scale(0.5).next_to(equ2, DOWN)
+        tangent = Text("Tangent : Equating the lowest degree term to").scale(0.5).to_edge(LEFT * 3)
+        lowest = Text("Zero And Differentiating").scale(0.5).next_to(tangent, DOWN)
+        equ = Text("(a² * y²) + (2 * a² * x²)= 0").scale(0.5).next_to(lowest, DOWN)
+        equ2 = Text("y² = -2*x²").scale(0.5).next_to(equ, DOWN)
+        equ3 = Text("""Therefore y is imaginary and 
+            there exist no tangent at (0,0)""").scale(0.5).next_to(equ2, DOWN)
         equ4 = Text("In Differentiation dx/dy ==> infinity ").scale(0.5).next_to(equ3, DOWN)
         final_equ = Text("Tangent at (a,0) and (2a,0) are paraell to y axis").scale(0.5).next_to(equ4, DOWN)
         self.play(Write(tangent))
@@ -107,7 +108,7 @@ class curve(Scene):
         asymptote = Text("Asymptote :").scale(0.4).to_edge(LEFT * 3)
         asym_xaxis = Text("||el to x-axis : coefficient of highest degree term of x equal to Zero").scale(0.4).next_to(asymptote, DR)
         asymp_x = Text("1=0 which is not possible , no asymptote ||el to x axis ").scale(0.4).next_to(asym_xaxis, DOWN)
-        asym_yaxis = Text("||el to y-axis : coefficient of highest degree term of y equal to Zero").scale(0.4).next_to(asymp_x, DR)
+        asym_yaxis = Text("||el to y-axis : coefficient of highest degree term of y equal to Zero").scale(0.4).next_to(asymp_x, DOWN)
         asymp_y = Text("a^2=0 which is not possible , no asymptote ||el to y axis ").scale(0.4).next_to(asym_yaxis, DOWN)
         x_final = Text("Therefore, no asymptote ||el to x-axis").scale(0.4).next_to(asymp_y, DOWN)
         self.play(Write(asymptote))
@@ -151,7 +152,7 @@ class curve(Scene):
         self.play(Unwrite(y_final))'''
 
         region = Text("Region of Absence : ").scale(0.4).to_edge(LEFT * 2.2)
-        curve_exists = Text("When x<a & x>2a curve does not exists").scale(0.4).next_to(region, DOWN)
+        curve_exists = Text("When x<a & x>2a curve does not exists").scale(0.4).next_to(region, DR)
         presence = Text("The curve exists in region a<x<2a").scale(0.4).next_to(curve_exists, DOWN)
         
         self.play(Write(region))
@@ -175,8 +176,8 @@ class curve(Scene):
         self.play(Uncreate(intersection_point2),Unwrite(intersection_label2))
         self.play(FadeOut(x_axis))
         #self.play(Uncreate(x_2a), Unwrite(asymptote_label))
-        self.play(question_text.animate.shift(LEFT*3.5))
-
+        self.play(question_text.animate.shift(LEFT*3 + UP*0.5) # change
+) 
         def implicit_function(x, y):
             return (a**2 * y**2) - (x**2)*(2*a - x) * (x-a)
         
@@ -187,15 +188,34 @@ class curve(Scene):
         axes = Axes(x_range=[-6,6], y_range=[-6,6], axis_config={"color":WHITE})
         self.play(Create(axes))
 
-        intersection_point = Dot(color=RED)
+        """intersection_point = Dot(color=RED)
         intersection_label = Tex("(0, 0)").scale(0.4).next_to(intersection_point, DL)
         self.play(Create(intersection_point),Write(intersection_label))
         intersection_point1 = Dot(color=RED)
-        intersection_label1 = Tex("(a, 0)").scale(0.4).next_to(intersection_point1, DL)
+        intersection_label1 = Tex("(a, 0)").scale(0.4).next_to(intersection_point1, DOWN+LEFT)
         self.play(Create(intersection_point1),Write(intersection_label1))
+        intersection_point2 = Dot(color=RED)
+        intersection_label2 = Tex("(2a, 0)").scale(0.4).next_to(intersection_point2, DOWN+LEFT)
+        self.play(Create(intersection_point2),Write(intersection_label2))"""
+
+#newline
         intersection_point = Dot(color=RED)
-        intersection_label2 = Tex("(0, 0)").scale(0.4).next_to(intersection_point2, DL)
+        intersection_point.move_to(axes.c2p(0,0))
+        intersection_label = Tex("(0, 0)").scale(0.4).next_to(intersection_point, DR)
+        self.play(Create(intersection_point),Write(intersection_label))
+        self.wait(1.5)
+
+        intersection_point1 = Dot(color=RED)
+        intersection_point1.move_to(axes.c2p(1,0))
+        intersection_label1 = Tex("(a, 0)").scale(0.4).next_to(intersection_point1, DR)
+        self.play(Create(intersection_point1),Write(intersection_label1))
+        self.wait(1.5)
+
+        intersection_point2 = Dot(color=RED)
+        intersection_point2.move_to(axes.c2p(2,0))
+        intersection_label2 = Tex("(2a, 0)").scale(0.4).next_to(intersection_point2, DR)
         self.play(Create(intersection_point2),Write(intersection_label2))
+        self.wait(1.5)
 
         '''tangent_POI = Line(start=LEFT * 6, end=RIGHT * 6, color=GREEN)
         tangent_label = Tex("y=0").scale(0.5).next_to(tangent_POI, UL)
