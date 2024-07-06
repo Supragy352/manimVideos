3b3992b71e46d8e66015ed59014c8c3b2fd5c132
Anooj29 <154309295+Anooj29@users.noreply.github.com>
2024-07-06 14:55:05 +0530

    commit

--- 3d.py
+++ 3d.py
@@ -1,16 +1,49 @@
-from manim import *
-from moderngl_window.meta import scene
-from numpy import array
+from queue import PriorityQueue
 
-class ThreeDGraph(Scene):
-	def construct(self):
+def solve_puzzle(initial_state):
+    # Define heuristic function (Manhattan distance)
+    def heuristic(state):
+        # Calculate Manhattan distance for each tile
+        # Return sum of distances
+        pass
 
-		ax = ThreeDAxes(
-			x_range=[-10, 10, 1],
-			y_range=[-10, 10, 1],
-			z_range=[-10, 10, 1]
-			)
-		graph = ax.get_graph()
+    # A* algorithm
+    frontier = PriorityQueue()
+    frontier.put((0, initial_state))  # (priority, state)
+    came_from = {}  # Dictionary to store parent of each state
+    cost_so_far = {}  # Dictionary to store cost of reaching each state
+    came_from[initial_state] = None
+    cost_so_far[initial_state] = 0
 
-		self.add((ax))
-		self.wait(5)
+    while not frontier.empty():
+        _, current_state = frontier.get()
+
+        if is_goal_state(current_state):
+            return reconstruct_path(came_from, current_state)
+
+        for next_state in get_possible_moves(current_state):
+            new_cost = cost_so_far[current_state] + 1  # Cost for each move is 1
+            if next_state not in cost_so_far or new_cost < cost_so_far[next_state]:
+                cost_so_far[next_state] = new_cost
+                priority = new_cost + heuristic(next_state)
+                frontier.put((priority, next_state))
+                came_from[next_state] = current_state
+
+    return None  # No solution found
+
+def is_goal_state(state):
+    # Check if the state is the goal state
+    pass
+
+def get_possible_moves(state):
+    # Generate possible moves from the current state
+    pass
+
+def reconstruct_path(came_from, current_state):
+    # Reconstruct and return the path from the initial state to the goal state
+    pass
+
+# Test the solver
+initial_state = [[1, 2, 3], [4, 0, 5], [6, 7, 8]]  # Initial state of the puzzle
+solution = solve_puzzle(initial_state)
+print("Solution:", solution)
